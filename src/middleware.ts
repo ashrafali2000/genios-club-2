e18ba62272ad6d7620a319b0f354165795f28b78
ashrafali2000 <ali4282271@gmail.com>
2024-05-31 14:31:12 +0500

    remove PK add USA

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,13 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
-export function middleware(request:NextRequest) {
-  const blockedCountries = ['PK']; // Example: Block USA and Germany
-  const country = request.geo?.country || request.headers.get('x-vercel-ip-country');
-  console.log(country,'country>>');
-  
-  if (blockedCountries.includes(country || '')) {
-    return new Response('Access Denied', { status: 403 });
+export function middleware(request: NextRequest) {
+  const blockedCountries = ["USA"]; // Example: Block USA and Germany
+  const country =
+    request.geo?.country || request.headers.get("x-vercel-ip-country");
+  console.log(country, "country>>");
+
+  if (blockedCountries.includes(country || "")) {
+    return new Response("Access Denied", { status: 403 });
   }
   return NextResponse.next();
 }
