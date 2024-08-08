14277a0fc0a3accdde929a4b03b05dd368da48ed
Zubair <57180013+MZubairLohar@users.noreply.github.com>
2024-08-09 00:18:50 +0500

    Update constant.ts

--- src/lib/constant.ts
+++ src/lib/constant.ts
@@ -6,12 +6,12 @@ export const clientId = "d75935c2db33dd391882dd2fb2474ceb"; // You can get a cli
 export const AddressZero = "0x0000000000000000000000000000000000000000";
 export const MTKAddress = "0x5F620d60795A2D0210c43bB927b2c989E1fbbf1c";
 // USDT new
-export const MTKAddress2 = "0xEAA16a292F9a0eaa4be9E68af4aC84fbD04B440E";
+export const MTKAddress2 = "0x3fCAF94585D3043aA4c30D0aA1B9e0c9E33f2240";
 export const PoolAddress = "0x456c66b4ea2246166d81772bC9Cc3e9F1D28ee06";
 // Mainnet
 export const GeniosClubAddress = "0x18642729d5770b18c108D0B991903e857f04497E";
 // MLM new
-export const GeniosClubAddress2 = "0xb7aeE81231a561e6dCA9122bC534809f862228Fb";
+export const GeniosClubAddress2 = "0xa89D3Bd7495deA5C32Cefa9d20fEb0943d0e3E64";
 
 export const MTKAbi = [
   {
